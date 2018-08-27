61b1302dc1d552ff27b403cba09b21e194549535
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-27 17:35:41 -0500

    countdown timer

--- app/src/main/java/xyz/camiloarguello/braintrainer/MainActivity.java
+++ app/src/main/java/xyz/camiloarguello/braintrainer/MainActivity.java
@@ -1,12 +1,17 @@
 package xyz.camiloarguello.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView secondsLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,23 @@ public class MainActivity extends AppCompatActivity {
     }
     public void startGame(View view){
         LinearLayout startContainer = findViewById(R.id.startContainer);
+        startContainer.animate().translationY(-1200f).setDuration(500);
 
-        startContainer.animate().translationYBy(-1000f).setDuration(500);
-        
+        secondsLeft = findViewById(R.id.secondsleft);
+        CountDownTimer countDown = new CountDownTimer(30000, 1000){
+
+            @Override
+            public void onTick(long l) {
+                Log.i("SECONDS", Long.toString(l));
+            }
+
+            @Override
+            public void onFinish() {
+               Log.i("FINISH", "Finished");
+            }
+        }.start();
+
+
+
     }
 }
