44c4129545cc1d7e4e2e14b486b526bcda328671
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-27 17:23:46 -0500

    layout finished

--- app/src/main/java/xyz/camiloarguello/braintrainer/MainActivity.java
+++ app/src/main/java/xyz/camiloarguello/braintrainer/MainActivity.java
@@ -2,6 +2,9 @@ package xyz.camiloarguello.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startGame(View view){
+        LinearLayout startContainer = findViewById(R.id.startContainer);
+
+        startContainer.animate().translationYBy(-1000f).setDuration(500);
+        
     }
 }
